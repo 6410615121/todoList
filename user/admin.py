9b03bad31a9f60245504df79229913c93e242e17
suttha jongjaroen <6410615121@cn330>
2023-11-10 09:40:00 +0700

    friend admin

--- user/admin.py
+++ user/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import todoUser ,Friend_request
 # Register your models here.
-admin.site.register(todoUser)
+class todoUserAdmin(admin.ModelAdmin):
+    filter_horizontal = ('friends',)  # This allows for a user-friendly way to select friends via a multi-select box
+admin.site.register(todoUser, todoUserAdmin)
 admin.site.register(Friend_request)
