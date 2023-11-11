77ce26c59a64536836b7bc2145f8e88b7764c1ef
nipitpon kampolrat <nipitpon@email.com>
2023-11-12 03:15:48 +0700

    update css

--- task/urls.py
+++ task/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('tasklist/', views.individual_tasklist, name='individual_tasklist'),   # link to individual_tasklist in views.py
-    path('taskadd/', views.task_add, name='task_add'),   # link to task_add in views.py
+    path('taskadd/<uuid:project_ID>/', views.task_add, name='task_add'),   # link to task_add in views.py
     path('individual_taskAdd/', views.individual_taskAdd, name = "individual_taskadd"), # link to individual_taskadd in views.py
     path('pastduetask/', views.individual_past_tasklist, name = "individual_past_tasklist"),
     path('completedtask/', views.individual_com_tasklist, name = "individual_com_tasklist"),
