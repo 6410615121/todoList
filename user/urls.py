2c9028a58e75139707ab2733e0fa7089145abae6
nipitpon kampolrat <nipitpon@email.com>
2023-11-16 15:09:09 +0700

    add function add friends

--- user/urls.py
+++ user/urls.py
@@ -7,6 +7,10 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout', views.logout_view, name='logout'),
     path('homepage/', views.homepage, name='homepage'),
-    path('friendrequest/<uuid:userID>/' ,views.send_friend_request ,name="send_friend_request"),
-    path('acceptfriend/<uuid:userID>/' ,views.accept_friend_request ,name="accept_friend_request"),
+    path('sendfriendrequest/<uuid:userID>/' ,views.send_friend_request ,name="send_friend_request"),
+    path('acceptfriend/' ,views.accept_friend_request ,name="accept_friend_request"),
+    path('myfriends/' ,views.friend_list ,name="friend_list"),
+    path('searchuser/' ,views.find_user ,name="find_user"),
+    path('friendrequest/' ,views.friend_request ,name="friend request"),
+    path('sendrequest/' ,views.show_send_request ,name="show_send_request"),
 ]
--- user/views.py
+++ user/views.py
@@ -23,7 +23,6 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, 'Successfully logged in.')
             return HttpResponseRedirect(reverse('homepage'))
         else:
             messages.error(request, 'Invalid username or password')
@@ -52,23 +51,77 @@ def register(request):
     return render(request, 'user/register.html', {'form': form})
 
 
+def friend_request(request):   # this function will render html that show all request from other users send request to me 
+    to_user = todoUser.objects.get(user=request.user)
+    
+    all_friend_request =  Friend_request.objects.filter(To_user = to_user)
+    context = {
+        'friends_request': all_friend_request,  
+    }
+    return render(request,'user/acc_friendrequest.html',context)
+
+
+
+def show_send_request(request):
+    from_user = todoUser.objects.get(user=request.user)
+    all_friend_request =  Friend_request.objects.filter(From_user = from_user)
+    context = {
+        'friends_request': all_friend_request,  
+        }
+    return render(request,'user/sendfriendrequest.html',context)
+
+
+
+
+
 def send_friend_request(request ,userID):
     from_user = todoUser.objects.get(user=request.user)
     to_user = todoUser.objects.get(todoUser_ID=userID)
-    friend_request ,created = Friend_request.objects.get_or_create( From_user = from_user ,To_user = to_user )
-    if created:
-        return redirect('homepage')
-    else:
-        return render(request, 'user/already_sentrequest.html')
+    Friend_request.objects.get_or_create( From_user = from_user ,To_user = to_user )
+    
+    return show_send_request(request)
+    
+    
 
 
-def accept_friend_request(request ,userID):
-    friend_request = Friend_request.objects.get(From_user = userID)
+def accept_friend_request(request):
+    
     user_profile = request.user
     id = todoUser.objects.get(user=user_profile)
+    friend_request = Friend_request.objects.get(To_user = id)  # other users send to me 
     if friend_request.To_user == id:
         friend_request.To_user.friends.add(friend_request.From_user)
         friend_request.From_user.friends.add(friend_request.To_user)
         friend_request.delete()
-        return render(request, 'user/register.html') # waiting html
+        return friend_list(request)
+        
     
+
+def friend_list(request):
+    myfriend = todoUser.objects.get(user=request.user).friends.all()
+    user_profile = request.user
+    myuser = todoUser.objects.get(user=user_profile)
+    context = {
+        'friends': myfriend,  
+        'myuser' : myuser
+    }
+    return render(request, 'user/friendlist.html',context)
+
+
+def find_user(request):
+    if request.method == 'POST':
+        user = request.POST.get('user')
+        try:
+            user_instance = User.objects.get(username=user)
+            user = todoUser.objects.get(user= user_instance)
+            context = {
+                'user': user,  
+        }
+            return render(request, 'user/detailuser.html',context)
+        except:
+            messages.error(request, 'There is no user')
+
+        
+
+
+    return render(request, 'user/finduser.html')
