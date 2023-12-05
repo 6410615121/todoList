5c09e19af673f22b33b4cdba9eeb41f10a8aad0a
suttha jongjaroen <6410615121@cn330>
2023-12-05 15:47:43 +0700

    add error handler

--- task/custom_context.py
+++ task/custom_context.py
@@ -9,7 +9,11 @@ def my_data_processor(request):
         return {'notification': []}
 
     # Now you can use actual_user to get the todoUser
-    to_user = todoUser.objects.get(user=request.user)
+    try:
+        to_user = todoUser.objects.get(user=request.user)
+    except todoUser.DoesNotExist:
+        # Create a new todoUser object for the user
+        to_user = todoUser.objects.create(user=request.user)
     all_friend_request = Friend_request.objects.filter(To_user=to_user)
 
     # Ensure that notification is always an iterable
